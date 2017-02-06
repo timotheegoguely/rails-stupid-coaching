2e854deecb90f3a5029e6e3f62da9a7048b45dbb
Timothee Goguely <timotheegoguely@gmail.com>
2017-02-06 14:05:06 +0100

    add coach answer

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,9 +1,34 @@
 class CoachingController < ApplicationController
   def ask
-
   end
 
   def answer
     @question = params[:q]
+    @answer = coach_answer(@question)
+  end
+
+  private
+
+  def coach_answer(question)
+    if question.downcase == "I am going to work right now!"
+      ''
+    elsif question.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don’t care, get dressed and go to work!"
+    end
+  end
+
+  def coach_answer_enhanced(question)
+    @answer = coach_answer(question)
+    if answer != ""
+      if question.upcase == question
+        "I can feel your motivation! #{@answer}"
+      else
+        answer
+      end
+    else
+      ""
+    end
   end
 end
